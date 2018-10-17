3dd23fe47d65ef181d8806b9b57cbbf3008e2b44
Ed Phelps <edphelps1@gmail.com>
2018-10-17 14:51:55 -0600

    server working

---
+++ controller.js
@@ -0,0 +1,76 @@
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const view = require('./view');
+
+// setup middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// setup static html folder
+// app.use(express.static('public'));
+
+// routes
+app.use('/movies', view);
+
+// ===========================================================
+// 404
+// ===========================================================
+app.use((req, res, next) => {
+  console.log("=========== 404 ==========");
+  res.status(404).json({ message: "Page not found" });
+  next();
+});
+
+/* **************************************************
+*  restructureError()
+*  @param Error -- actual Error object
+*  Returns object { status: 123, message: 'xxxx', stack: 'xxx' }
+***************************************************** */
+function restructureError(error) {
+  // return if error not in the expected form
+  if (!error.stack)
+    return error;
+
+  // look for ' at ' seperating error message from call stack
+  const i = error.stack.search(' at ');
+  if (i === -1)
+    return { message: error.message, stack: 'undetermined', status: error.status };
+
+  // parse the call_stack portion of the string from end of error.stack
+  const restructured = {
+    error: {
+      message: error.message,
+      stack: error.stack.slice(i + 4),
+    },
+  };
+  if (error.status)
+    restructured.error.status = error.status;
+  return restructured;
+}
+
+// ===========================================================
+// Error handler for next(object) / 500
+// ===========================================================
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  console.log("======================= APP ERROR =======================");
+  console.log(err);
+  console.log("^^^^^^^^^^^^^^^^^^^^^^^ APP ERROR ^^^^^^^^^^^^^^^^^^^^^^");
+  res.status(status).json(restructureError(err));
+  next();
+});
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Bookstore API listening on port ${port}!`);
+  });
+}
+
+module.exports = app;
---
+++ view.js
@@ -0,0 +1,136 @@
+const express = require('express');
+
+const router = express.Router();
+const model = require('./model.js');
+
+const { chkBodyParams } = require('./params'); // destructure the chkBodyParams out of require('./params') returned object
+
+/* **************************************************
+*  POST /movies
+*  Add a new movie
+*  @body title
+*  @body director
+*  @body year (int)
+*  @body rating (int)
+*  @body poster (url to a poster)
+*  @return the new movie record with id or forwards w/ next(Error)
+http POST localhost:3000/movies title='My title' director='director' year=2000 rating=5 poster='http://blah-blah'
+***************************************************** */
+router.post('', (req, res, next) => {
+  const oParams = {
+    title: 'string',
+    director: 'string',
+    year: 'int',
+    rating: 'int',
+    poster: 'string',
+  };
+  if (!chkBodyParams(oParams, req, res, next))
+    return;
+  const oMovie = {
+    // id: not-passed-to-create-new-record
+    title: req.body.title,
+    director: req.body.director,
+    year: req.body.year,
+    rating: req.body.rating,
+    poster: req.body.poster,
+  };
+  model.create(oMovie)
+    .then((newMovie) => {
+      res.status(201).json(newMovie);
+    })
+    .catch((error) => {
+      error.status = 400;
+      next(error);
+    });
+});
+
+/* **************************************************
+*  GET /movies
+*  Return array of all movies
+http GET localhost:3000/movies
+***************************************************** */
+router.get('', (req, res, next) => {
+  model.readAll()
+    .then((aMovies) => {
+      res.status(200).json(aMovies);
+    })
+    .catch((error) => {
+      error.status = 400;
+      next(error);
+    });
+});
+
+/* **************************************************
+*  GET /movies/:id
+*  Return the movie
+http GET localhost:3000/movies/2
+***************************************************** */
+router.get('/:id', (req, res, next) => {
+  model.read(req.params.id)
+    .then((oMovie) => {
+      res.status(200).json(oMovie);
+    })
+    .catch((error) => {
+      error.status = 404;
+      next(error);
+    });
+});
+
+/* **************************************************
+*  PUT /movies/:id
+*  Update the movie
+*  @body title
+*  @body director
+*  @body year (int)
+*  @body rating (int)
+*  @body poster (url to a poster)
+*  Return, the updated movie record
+http PUT localhost:3000/movies/2  title='My title' director='director' year=2000 rating=5 poster='http://blah-blah'
+***************************************************** */
+router.put('/:id', (req, res, next) => {
+  const oParams = {
+    title: 'string',
+    director: 'string',
+    year: 'int',
+    rating: 'int',
+    poster: 'string',
+  };
+  if (!chkBodyParams(oParams, req, res, next))
+    return;
+  const oMovie = {
+    id: req.params.id,
+    title: req.body.title,
+    director: req.body.director,
+    year: req.body.year,
+    rating: req.body.rating,
+    poster: req.body.poster,
+  };
+  model.update(oMovie)
+    .then((updatedMovie) => {
+      res.status(201).json(updatedMovie);
+    })
+    .catch((error) => {
+      error.status = 400;
+      next(error);
+    });
+});
+
+/* **************************************************
+*  DELETE /movies/:id
+*  Delete the movie
+*  Return, the deleted record
+http DELETE localhost:3000/movies/2
+***************************************************** */
+router.delete('/:id', (req, res, next) => {
+  model.destroy(req.params.id)
+    .then((oMovie) => {
+      res.status(200).json(oMovie);
+    })
+    .catch((error) => {
+      error.status = 404;
+      next(error);
+    });
+});
+
+
+module.exports = router;
